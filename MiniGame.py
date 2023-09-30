258d270dea9934547f2a66165c4b409707bc5935
Dara Elisa Praça <Daraelisac@gmail.com>
2023-09-30 14:35:43 -0300

    first commit

---
+++ MiniGame.py
@@ -0,0 +1,11 @@
+palpite = 2 
+numero = 2
+
+while palpite != numero:
+    print("Advinhe qual numero o numero correto? ")
+    palpite = int(input())
+
+
+#print("Parabens voce acertou")
+#print("Você errou")
+print(bool(3>8))
